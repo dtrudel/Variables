90ebd9fff0b98af419ee5cd52427b060918a0ced
david <dtrudel0505@gmail.com>
2016-10-25 15:58:03 -0400

    first commit

---
+++ src/variables.java
@@ -0,0 +1,12 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		int a=63;
+				int b=45;
+				a = a + 10;
+				System.out.println("a is" + a);
+				System.out.println("b is" + b);
+	}
+
+}
