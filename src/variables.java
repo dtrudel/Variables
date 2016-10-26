6e4529de9b350ec9b0efff8015f490627135c6e6
david <dtrudel0505@gmail.com>
2016-10-26 09:17:10 -0400

    improved documentation

--- src/variables.java
+++ src/variables.java
@@ -4,9 +4,16 @@ public class variables {
 	public static void main(String[] args) {
 		int a=63;
 				int b=45;
-				a = a + 10;
+				//a = a + 10;
+				//a += 10;
+				//a++;
+				//a += 1;
+				// here's a comment
+				// here's another comment
+				
 				System.out.println("a is" + a);
 				System.out.println("b is" + b);
+				
 	}
 
 }
